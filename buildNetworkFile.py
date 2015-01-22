4e269ba45106254c05ca4aaa4038f9841bb9f174
Chris Cox <crcox@wisc.edu>
2015-01-22 12:58:41 -0600

    accomodate expdir config option

--- buildNetworkFile.py
+++ buildNetworkFile.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 from lensapi import network
 import json
+import os
 
 with open('network.json','rb') as f:
     jdat = json.load(f)
+    try:
+        edir = jdat['expdir']
+    except KeyError:
+        edir = ''
+    try:
+        os.makedirs(edir)
+    except OSError:
+        pass
     filename = '.'.join([jdat["name"],'in'])
-    with open(filename,'w') as f:
+    fpath = os.path.join(edir,filename)
+    with open(fpath,'w') as f:
         network.writeNetworkDefinition(jdat,f)
         f.write('\n');
         network.writeLayerDefinitions(jdat,f)
