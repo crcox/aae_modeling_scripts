f3660d169fd1cdcb7ec5ad0453e85bb689dd5c0c
ChrisCox <crcox@wisc.edu>
2015-05-20 07:51:37 -0500

    Fixed detectSpike

--- tcl/detectSpike.tcl
+++ tcl/detectSpike.tcl
@@ -1,17 +1,18 @@
-proc detectSpike {errHistory err} {
-	set n [llength errHistory]
+proc detectSpike {errHistory err {threshold 3}} {
+	set n [llength $errHistory]
 	set sum 0
 	foreach e $errHistory {set sum [expr {$sum + $e}]}
 	set mean [expr {$sum / $n}]
 	set var 0
 	foreach e $errHistory {
 		set diff [expr {$mean - $e}]
-		set var [expr {$var + $diff*$diff}]
+		set diff2 [expr {$diff * $diff}]
+		set var [expr {$var + $diff2}]
 	}
 	set x [expr {$var / $n}]
 	set std [expr sqrt($x)]
 
-	set crit [expr {$mean + 2*$std}]
+	set crit [expr {$mean + ($threshold*$std)}]
 	puts $crit
 	puts $err
 	if { $err > $crit } {
